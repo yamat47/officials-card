7f449d642c74a22b42437a4bde5cdd1c4e1a0f73
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2020-12-31 13:49:47 +0900

    バリデーションに引っかかったときはPDFを生成させない。

--- app/controllers/official_cards_controller.rb
+++ app/controllers/official_cards_controller.rb
@@ -4,4 +4,21 @@ class OfficialCardsController < ApplicationController
   def new
     @form = OfficialCardForm.new
   end
+
+  def create
+    @form = OfficialCardForm.new(official_card_params)
+
+    if @form.generate_pdf
+      raise
+    else
+      render :new
+    end
+  end
+
+  private
+    def official_card_params
+      params
+        .require(:official_card_form)
+        .permit(:tournament, :date, :field, :home, :visitor)
+    end
 end
--- app/forms/official_card_form.rb
+++ app/forms/official_card_form.rb
@@ -10,4 +10,14 @@ class OfficialCardForm
   attribute :field, :string
   attribute :home, :string
   attribute :visitor, :string
+
+  validates :tournament, presence: true
+  validates :date, presence: true
+  validates :field, presence: true
+  validates :home, presence: true
+  validates :visitor, presence: true
+
+  def generate_pdf
+    return false unless valid?
+  end
 end
